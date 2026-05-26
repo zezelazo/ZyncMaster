b619b89b088402cdf6779ff1b8280e5a1e810d80
zezelazo <zezelazo@msn.com>
2026-05-26 11:16:25 -0500

    Add Microsoft account connect and callback endpoints

--- src/SyncMaster.Server/Program.cs
+++ src/SyncMaster.Server/Program.cs
@@ -2,9 +2,15 @@ using SyncMaster.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<ServerOptions>(builder.Configuration.GetSection("Server"));
+builder.Services.AddDataProtection();
+
 builder.Services.AddSingleton<IDeviceStore, InMemoryDeviceStore>();
 builder.Services.AddSingleton<ISyncStateStore, InMemorySyncStateStore>();
+builder.Services.AddSingleton<ISecretProvider, ConfigurationSecretProvider>();
+builder.Services.AddSingleton<IConnectedAccountStore, DataProtectionConnectedAccountStore>();
 builder.Services.AddScoped<DeviceService>();
+builder.Services.AddHttpClient<IMicrosoftTokenService, MicrosoftTokenService>();
 builder.Services.AddAuthentication("ApiKey").AddApiKeyAuth();
 builder.Services.AddAuthorization();
 
@@ -16,6 +22,7 @@ app.UseAuthorization();
 app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
 
 app.MapDeviceEndpoints();
+app.MapConnectEndpoints();
 
 app.Run();
 
